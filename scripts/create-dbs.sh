236f6c7d36b4c1f8c3486c0f78e4556aaca55a5e
skxwave <kovalchuk.roman05@gmail.com>
2026-03-09 21:18:31 +0200

    fix: db creation for infra compose added

--- scripts/create-dbs.sh
+++ scripts/create-dbs.sh
@@ -5,9 +5,10 @@ set -u
 
 function create_user_and_database() {
 	local database=$1
-	echo "  Creating database '$database'"
+	echo "  Ensuring database '$database' exists"
 	psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
-	    CREATE DATABASE $database;
+	    SELECT 'CREATE DATABASE $database'
+	    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$database')\gexec
 	EOSQL
 }
 
